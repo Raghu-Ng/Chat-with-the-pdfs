35c02e3d87605be1961d107a40a0cb88b35740d3
Raghava Reddy N <raghau.20003@gmail.com>
2024-10-02 22:00:34 +0530

    large model

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vectorstore):
     # llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id="google/flan-t5-base", model_kwargs={"temperature":0.5, "max_length":512})
+    llm = HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":0.5, "max_length":512})
     memory = ConversationBufferMemory(memory_key = 'chat_history', return_messages = True )
     Conversation_chain = ConversationalRetrievalChain.from_llm(
         llm = llm, 
